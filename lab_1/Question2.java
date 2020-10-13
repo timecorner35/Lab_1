a74eaa96afe93a28c0ad70b043272d173d973015
timecorner35 <71405627+timecorner35@users.noreply.github.com>
2020-10-13 12:29:41 +0100

    Update Question2.java

--- lab_1/Question2.java
+++ lab_1/Question2.java
@@ -7,7 +7,7 @@ public class Question2 {
 		numbers(8, 7);
 	}
 	public static void numbers(int num1, int num2) {
-		String result = ((num1==6)||(num2==6)) ? "true" : "false";
+		String result = ((num1==6)||(num2==6)||(num1+num2==6)||(num1-num2==6)||(num2-num2==6)) ? "true" : "false";
 		System.out.println(result);
 	}
 
